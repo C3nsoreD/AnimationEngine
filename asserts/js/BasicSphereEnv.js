d3e0a88166a8938ec47102d76b94cedb186dae6f
Ivan Richard <ugoivan@gmail.com>
2019-08-25 12:46:31 -0400

    adding more textures and fixing the wireframe bug

--- asserts/js/BasicSphereEnv.js
+++ asserts/js/BasicSphereEnv.js
@@ -2,11 +2,17 @@
 var artifact = 'fbx/Sphere.fbx';
 var container = 'basicSphereEnv';
 var datContainer = '.datGUI';
+var wireActive = false;
 
 var textures = [
     'texture/bluecrack.jpg',
     'texture/Bluefeathers.jpg',
-    'texture/blueOcean.jpg'
+    'texture/blueOcean.jpg',
+    'texture/blueWaveSkin.jpg',
+    'texture/camoskin.jpg',
+    'texture/darkbear.jpg',
+    'texture/greenleaf.jpg',
+    'texture/icetexture.jpg'
 ];
 
 var datGUI = new dat.GUI({ autoPlace: false});
@@ -37,8 +43,9 @@ datGUI.add(params, 'animation').min(0).max(1).step(1).onFinishChange(function(){
 });
 
 //adding the texture slider
-datGUI.add(params, 'texture').min(0).max(2).step(1).onFinishChange(function(){
+datGUI.add(params, 'texture').min(0).max(7).step(1).onFinishChange(function(){
     basicSphereEnv.update({texture : params.texture});
+    wireActive = true;
 });
 
 //adding the pause button
@@ -49,10 +56,15 @@ datGUI.add(params, 'paused').onFinishChange(function(){
         basicSphereEnv.resumeAnimation();
 }); 
 
-//adding the wireframe option
-datGUI.add(params, 'wireframe').onFinishChange(function(){
-    basicSphereEnv.update({wireframe : params.wireframe});   
-});
+
+//if(wireActive == true){
+    //adding the wireframe option
+    datGUI.add(params, 'wireframe').onFinishChange(function(){
+        basicSphereEnv.update({texture : params.texture});
+        basicSphereEnv.update({wireframe : params.wireframe});   
+    });
+//}
+
 
 //add datGUI to the dom Element
 $(datContainer).append($(datGUI.domElement));
