e6eeab38f91a1e855e14cb044d11b085ae80fa04
Ivan Richard <ugoivan@gmail.com>
2019-10-14 01:33:30 -0400

    more updates to the naming and add a way to swap avatars

--- asserts/js/Start.js
+++ asserts/js/Start.js
@@ -1,7 +1,8 @@
-
-var artifact = 'fbx/DragonFinal.fbx';
-var container = 'basicSphereEnv';
+var container = 'env';
 var datContainer = '.datGUI';
+
+//adding an array of artifacts
+var artifacts = ['fbx/DragonFinal.fbx', 'fbx/Sphere.fbx'];
 
 var textures = [
     'texture/bluecrack.jpg',
@@ -18,48 +19,55 @@ var datGUI = new dat.GUI({ autoPlace: false});
 
 
 //create the environment variable
-var basicSphereEnv = new Environment( 
-    artifact,
+var env =  new Environment( 
+    artifacts,
     textures, 
     $('#'+container).width(),
     $('#'+container).height(),
-    container,0,-70,0);
+    container,0,-70,0
+);
 
 //run the environment.
-basicSphereEnv.run();
+env.run();
 
 //creating a list of parameters
 var params = {
+    artifact : 0,
     animation : 0,
     texture: 0,
     paused : false,
     wireframe: false
 };
 
+//ading the artifact switcher
+datGUI.add(params, 'artifact').min(0).max(artifacts.length-1).step(1).onFinishChange(function(){
+    console.log(params.artifact);
+    env.swapAvatar(params.artifact);    
+});
+
 //adding the animation switcher 
 datGUI.add(params, 'animation').min(0).max(1).step(1).onFinishChange(function(){
-    basicSphereEnv.update({animation: params.animation});
+    env.update({animation: params.animation});
 });
 
 //adding the texture slider
 datGUI.add(params, 'texture').min(0).max(7).step(1).onFinishChange(function(){
-    basicSphereEnv.update({texture : params.texture});
-    //wireActive = true;
+    env.update({texture : params.texture});
 });
 
 //adding the pause button
 datGUI.add(params, 'paused').onFinishChange(function(){
     if(params.paused) 
-        basicSphereEnv.pauseAnimation();
+        env.pauseAnimation();
     else
-        basicSphereEnv.resumeAnimation();
+        env.resumeAnimation();
 }); 
 
 
 //adding the wireframe option
 datGUI.add(params, 'wireframe').onFinishChange(function(){
-    basicSphereEnv.update({texture : params.texture});
-    basicSphereEnv.update({wireframe : params.wireframe});   
+    env.update({texture : params.texture});
+    env.update({wireframe : params.wireframe});   
 });
 
 
